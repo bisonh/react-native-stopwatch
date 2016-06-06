48bfe8ac60d23866e309ec102eb45acebdcd2d01
davidbison <davidbisonhubert@gmail.com>
2016-06-06 14:48:48 -0700

    Modify border color of start/stop button depending on state

--- index.ios.js
+++ index.ios.js
@@ -42,10 +42,12 @@ var StopWatch = React.createClass({
     </View>
   },
   startStopButton: function() {
+    var style = this.state.running ? styles.stopButton : styles.startButton;
+
     return <TouchableHighlight
       underlayColor="gray"
       onPress={this.handleStartPress}
-      style={[styles.button, styles.startButton]}>
+      style={[styles.button, style]}>
         <Text>
           {this.state.running ? 'Stop' : 'Start'}
         </Text>
@@ -112,6 +114,9 @@ var styles = StyleSheet.create({
   },
   startButton: {
     borderColor: '#00CC00'
+  },
+  stopButton: {
+    borderColor: '#CC0000'
   }
 });
 
